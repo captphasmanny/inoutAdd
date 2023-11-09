1677dd4e38c8ccf3d78558483ffbe3c37e721e7f
Manny <captphasmanny@att.net>
2023-11-09 08:33:13 -0500

    Initial Commit

---
+++ inoutAdd/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  inoutAdd
+//
+//  Created by Emanuel Alfaro on 11/9/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
