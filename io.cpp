ea1d965736c1088df4d417cc88fb70b54ce70444
Manny <captphasmanny@att.net>
2023-11-09 08:52:56 -0500

    Was able to break out the io functions out to separate files.

---
+++ io.cpp
@@ -0,0 +1,8 @@
+//
+//  io.cpp
+//  inoutAdd
+//
+//  Created by Emanuel Alfaro on 11/9/23.
+//
+
+#include "io.hpp"
---
+++ io.hpp
@@ -0,0 +1,13 @@
+//
+//  io.hpp
+//  inoutAdd
+//
+//  Created by Emanuel Alfaro on 11/9/23.
+//
+
+#ifndef io_hpp
+#define io_hpp
+
+#include <stdio.h>
+
+#endif /* io_hpp */
